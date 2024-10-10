edcc334797aaf789f98f62b1111b68a7c33715e1
Devious Cat <LeoMickelson@outlook.com>
2024-10-10 16:14:22 -0400

    This prob won't work :(((((((((((((((((((((((

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./layouts/**/*.html",
-		"./content/**/*.{html,md}",
-		"./themes/**/layouts/**/*.html",
-		"./themes/**/content/**/*.{html,md}",
-    "./hugo_stats.json"
-	],
+	content: ["./hugo_stats.json"],
 	theme: {
 		extend: {},
 	},

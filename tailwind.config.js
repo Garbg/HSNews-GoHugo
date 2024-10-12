b0b18bfeac3d9d12e8f038903e05005bca36d223
Devious Cat <LeoMickelson@outlook.com>
2024-10-11 20:34:39 -0400

    Fix color not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
-
+const primarycol = colors.blue["600"]
 module.exports = {
 	content: ["./layouts/**/*.html", "./themes/**/layouts/**/*.html"],
 	theme: {
 		colors: {
-			primary: colors.blue["600"],
+			primary: primarycol,
 		},
 		extend: {
 			typography: {
 				custom: {
 					css: {
 						"--tw-prose-body": colors.black,
-						'--tw-prose-links': colors.primary,
+						'--tw-prose-links': primarycol,
 					},
 				},
 			},

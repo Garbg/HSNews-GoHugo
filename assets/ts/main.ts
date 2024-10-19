1cf63903aaf027411a8d99ec9223449bc49f5dd4
leomick <83542885+leomick@users.noreply.github.com>
2024-10-19 18:00:31 -0400

    Update hljs prefs

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,7 +1,7 @@
 import hljs from 'highlight.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-	for (const el of document.querySelectorAll("code")) {
-		hljs.highlightElement(el);
-	}
-});
+hljs.highlightAll();
+//document.addEventListener("DOMContentLoaded", () => {
+//	for (const el of document.querySelectorAll("code")) {
+//		hljs.highlightElement(el);
+//	}
+//});
